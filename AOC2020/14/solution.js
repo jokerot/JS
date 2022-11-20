cd15ed9d2b1689a65730cf00c2c792ec6e55b40e
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2022-11-20 23:11:23 +0100

    ds

--- AOC2020/14/solution.js
+++ AOC2020/14/solution.js
@@ -8,15 +8,27 @@ function createBinaryString(nMask) {
     return sMask;
 }
 
+const maskIt = (m, code) => {
+    code = code.split('');
+    for (let i=0; i<32; i++){
+        if (m[i] != 'X') code[i] = m[i]
+    }
+    return code.join('')
+}
+
 const sol1 = input => {
     const regex = /mem\[(\d*)\] = (\d*)/g
-    const arr = []
+    let arr = []
+    let obj = {}
     for (let piece of input) {
         let p = piece.split('\n')
         let mask = p[0];
-        let codes = p.slice(1).flatMap(x => [...x.matchAll(regex)]).map
-        arr.push([mask, codes])
+        let codes = p.slice(1).flatMap(x => [...x.matchAll(regex)])
+        .map(y=> ({ [y[1]]: maskIt(mask, createBinaryString(y[2])) }))
+
+        arr = [...arr, ...codes]
     }
+    obj = arr.reduce((o, x) => ({...obj, []}))
     return arr
 
 }
