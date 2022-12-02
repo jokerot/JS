48628885d3fd29a801dd39bba25e80d303787d0c
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2022-12-02 04:03:13 +0100

    start 17 2021

---
+++ AOC2021/17/solution.js
@@ -0,0 +1,6 @@
+import { realInput, sampleInput, sampleInput2, sampleInput3, sampleInput4 } from "./input.js";
+const start = Date.now();
+
+
+
+console.log("Time spent: ", Date.now() - start, " ms")
