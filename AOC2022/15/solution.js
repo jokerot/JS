592fc54f0a2c5b03eb5f64dbc3c50aa72754a313
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2022-12-15 23:41:52 +0100

    2022 15 damn

--- AOC2022/15/solution.js
+++ AOC2022/15/solution.js
@@ -2,7 +2,16 @@ import { realInput, sampleInput } from "./input.js";
 console.time()
 const regex = /Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)/g
 
-const sensors1 = sampleInput.split('\n').map(x => {
+// const line1 = new Array(60).fill(".")
+const line1 = new Array(4000000).fill(".")
+const size = 4000000
+// const size = 20
+const linePos = 2000000
+// const linePos = 10
+let min = 0
+
+console.time('Part1 Time:')
+const sensors1 = realInput.split('\n').map(x => {
     return [...x.matchAll(regex)][0].slice(1).map(y => parseInt(y))
 })
 
@@ -15,78 +24,71 @@ const findSides = (x1, y1, x2, y2) => {
         [x1, y1 + radius, x1 - radius, y1],
         [x1 - radius, y1, x1, y1 - radius],
         [x1, y1 - radius, x1 + radius, y1]
-        ]
+    ]
     return s
 }
 
-const allSides1 = sensors1.reduce((s, x) => [...s,...findSides(x[0], x[1], x[2], x[3])], [])
+const allSides1 = sensors1.reduce((s, x) => [...s, ...findSides(x[0], x[1], x[2], x[3])], [])
 
 function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
 
     // Check if none of the lines are of length 0
-      if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
-          return false
-      }
-  
-      const denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
-  
+    if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+        return false
+    }
+
+    const denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+
     // Lines are parallel
-      if (denominator === 0) {
-          return false
-      }
-  
-      let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
-      let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
-  
+    if (denominator === 0) {
+        return false
+    }
+
+    let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator
+    let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator
+
     // is the intersection along the segments
-      if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
-          return false
-      }
-  
+    if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+        return false
+    }
+
     // Return a object with the x and y coordinates of the intersection
-      let x = x1 + ua * (x2 - x1)
-      let y = y1 + ua * (y2 - y1)
-  
-      return [Math.floor(x), Math.floor(y)]
-  }
+    let x = x1 + ua * (x2 - x1)
+    let y = y1 + ua * (y2 - y1)
+
+    return [x, y]
+}
 
 const findIntersections = () => {
     const intersections = []
-    for (let i = 0; i < allSides1.length -1 ; i++) {
+    for (let i = 0; i < allSides1.length - 1; i++) {
         const currentSide = allSides1.pop()
-        allSides1.forEach(e=>{
-            let [x1,y1,x2,y2] = currentSide;
-            let [x3,y3,x4,y4] = e;
-            let intersection = intersect(x1,y1,x2,y2,x3,y3,x4,y4)
+        allSides1.forEach(e => {
+            let [x1, y1, x2, y2] = currentSide;
+            let [x3, y3, x4, y4] = e;
+            let intersection = intersect(x1, y1, x2, y2, x3, y3, x4, y4)
             if (intersection) intersections.push(intersection)
         })
     }
-    return intersections.sort((a,b) => a[1] - b[1])
+    return intersections.sort((a, b) => a[1] - b[1])
 }
 
 const allIntersections = findIntersections()
-const pointsOfInterest = allIntersections.flatMap(x=>[[x[0]+1, x[0]], [x[0]-1, x[0]], [x[0], x[0]+1], [x[0], x[0]-1]])
+const pointsOfInterest = allIntersections.flatMap(x => [[x[0] + 1, x[1]], [x[0] - 1, x[1]], [x[0], x[1] + 1], [x[0], x[1] - 1]])
+    .filter(y => y[0] > 0 && y[0] < size && y[1] > 0 && y[1] < size)
 
-const inRange = ([x1,y1,x2,y2], [p1, p2]) => {
+const inRange = ([x1, y1, x2, y2], [p1, p2]) => {
     const dX = Math.abs(x1 - x2)
     const dY = Math.abs(y1 - y2)
     const radius = dX + dY
     const pX = Math.abs(x1 - p1)
     const pY = Math.abs(y1 - p2)
     const pRadius = pX + pY
-    return (pRadius<=radius)
+    const t = (pRadius <= radius)
+    return (pRadius <= radius)
 }
 
 
-let l = '.'
-
-// const line1 = new Array(60).fill(".")
-const line1 = new Array(4000000).fill(".")
-const size = 40000000
-// const size = 20
-const linePos = 2000000
-// const linePos = 10
-let min = 0
 const fillLine = (x1, y1, x2, y2) => {
     const dX = Math.abs(x1 - x2)
     const dY = Math.abs(y1 - y2)
@@ -103,28 +105,12 @@ const fillLine = (x1, y1, x2, y2) => {
     if (y2 == linePos) line1[x2] = 'B'
 }
 
-const fillLine2 = (x1, y1, x2, y2, pLine, l) => {
-    const dX = Math.abs(x1 - x2)
-    const dY = Math.abs(y1 - y2)
-    const radius = dX + dY
-    const cut = radius - (Math.abs(y1 - pLine))
-    if (cut > 0) {
-        for (let i = 0; i <= cut; i++) {
-            l[x1 + i] = '#'
-            l[x1 - i] = '#'
-            min = min < x1 - i ? min : x1 - i
-        }
-    }
-
-}
 
 
 const sol1 = () => {
     sensors1.forEach(e => {
         fillLine(e[0], e[1], e[2], e[3],)
     });
-
-    // console.log(line1.join(''))
 
     let count = 0
     for (let i = min; i < line1.length; i++) {
@@ -135,36 +121,25 @@ const sol1 = () => {
 }
 
 const sol2 = () => {
-    for (let i = 0; i < linePos; i++) {
-        const l1 = new Array(size).fill(".")
-        const l2 = new Array(size).fill(".")
-        sensors1.forEach(e => {
-            fillLine2(e[0], e[1], e[2], e[3], linePos + i, l1)
-        });
-        for (let j = 1; j < size - 1; j++) {
-            if ((l1[j - 1] + l1[j] + l1[j + 1]) == "#.#") {
-                return ["FOUND IT!!!", j, linePos + i]
-            }
+    for (let k = 0; k < pointsOfInterest.length; k++) {
 
-        }
-        sensors1.forEach(e => {
-            fillLine2(e[0], e[1], e[2], e[3], linePos - i, l2)
-        });
-        for (let j = 1; j < size - 1; j++) {
-            if ((l2[j - 1] + l2[j] + l2[j + 1]) == "#.#") {
-                return ["FOUND IT!!!", j, linePos - i]
+        let isDistress = true
+        for (let i = 0; i < sensors1.length; i++) {
+            if (inRange(sensors1[i], pointsOfInterest[k])) {
+                isDistress = false;
+                break;
             }
-
         }
-        console.log("Nope", linePos - i);
+        if (isDistress) {
+            console.log(pointsOfInterest[k])
+            return pointsOfInterest[k][0] * 4000000 + pointsOfInterest[k][1]
+        }
 
     }
-
-    // console.log(line1.join(''))
-    return "NOPE"
+    return "Finish"
 }
 
-console.time('Part1 Time:')
+
 console.log("Part 1: ", sol1());
 console.timeEnd('Part1 Time:')
 
