770e5c3b601529ed6e4249b8e4feeaf5be66711e
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2022-12-20 04:21:13 +0100

    slow solution to p1

--- AOC2022/16/solution.js
+++ AOC2022/16/solution.js
@@ -1,41 +1,46 @@
 import { realInput, sampleInput } from "./input.js";
 console.time()
 const regex = /Valve (\w\w) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.*)/g
-const valves = sampleInput.split('\n')
+const valves = realInput.split('\n')
     .map(x => [...x.matchAll(regex)][0].slice(1))
     .map(y => ([y[0], y[1], y[2].split(', ')]))
 
+let maxScore = 0
 const objValves = valves.reduce((obj, x) => ({ ...obj, [x[0]]: { value: x[1], connectedValves: x[2] } }), {})
-const allSolutions = []
+let allSolutions = []
 const nextStep = (currValve, movesLeft, lastValve, currentValvesObj, currentMovesArr, currScore) => {
     movesLeft--
     if (movesLeft <= 1) {
-        // console.log(currScore)
-        allSolutions.push([currScore, currentMovesArr])
+        if (maxScore < currScore) {
+            maxScore = currScore
+            console.log("Max1", maxScore)
+        }
+        allSolutions = currentMovesArr
         return
     }
 
-    if (currScore += currentValvesObj[currValve].value > 0) {
-        movesLeft--
-        currScore += currentValvesObj[currValve].value * movesLeft
-        const temp = currentValvesObj[currValve].value
-        currentValvesObj[currValve].value = 0
-        currentValvesObj[currValve].connectedValves.forEach(v => {
-            if (v != lastValve)
-                nextStep(v, movesLeft, currValve, { ...currentValvesObj }, [...currentMovesArr, v], currScore)
-        });
-        movesLeft++
-        currentValvesObj[currValve].value = temp
-    }
-
+    
     currentValvesObj[currValve].connectedValves.forEach(v => {
         if (v != lastValve)
-            nextStep(v, movesLeft, currValve, { ...currentValvesObj }, [...currentMovesArr, v], currScore)
+        nextStep(v, movesLeft, currValve, JSON.parse(JSON.stringify(currentValvesObj)), [...currentMovesArr, v], currScore)
     });
+    
+    if (currentValvesObj[currValve].value > 0) {
+        movesLeft--
+        currScore += currentValvesObj[currValve].value * movesLeft
+        const tempValvesObj = JSON.parse(JSON.stringify(currentValvesObj))
+        tempValvesObj[currValve].value = 0
+        currentValvesObj[currValve].connectedValves.forEach(v => {
+            nextStep(v, movesLeft, currValve, tempValvesObj, [...currentMovesArr, v], currScore)
+        });
+    }
 
     if (movesLeft <= 1) {
-        // console.log(currScore)
-        allSolutions.push([currScore, currentMovesArr])
+        if (maxScore < currScore) {
+            maxScore = currScore
+            console.log("Max2", maxScore)
+        }
+        allSolutions = currentMovesArr
         return
     }
 }
@@ -47,19 +52,10 @@ const checkValvesAllOpen = (v) => {
     return true
 }
 
-nextStep("DD", 30, 'AA', objValves, ["AA"], 0)
+nextStep("AA", 31, '', objValves, ["AA"], 0)
+console.log("FINAL ANSWER:", maxScore)
 
-allSolutions.sort((a,b) => a[0] - b[0])
+// allSolutions.sort((a, b) => a[0] - b[0])
 
-const sol1 = () => 1
-const sol2 = () => 1
-
-console.time('Part1 Time:')
-console.log("Part 1: ", sol1());
-console.timeEnd('Part1 Time:')
-
-console.time('Part2 Time:')
-console.log("Part 2: ", sol2());
-console.timeEnd('Part2 Time:')
 
 console.timeEnd()
